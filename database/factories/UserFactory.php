a7b369f61cadd4f1faf1fc6091f595ae0ef41cb7
misu571 <jubir.hosn@gmail.com>
2026-02-12 18:27:29 +0600

    fix user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -33,9 +33,6 @@ class UserFactory extends Factory
             'remember_token' => Str::random(10),
             'image' => null,
             'is_active' => true,
-            'two_factor_secret' => null,
-            'two_factor_recovery_codes' => null,
-            'two_factor_confirmed_at' => null,
         ];
     }
 
