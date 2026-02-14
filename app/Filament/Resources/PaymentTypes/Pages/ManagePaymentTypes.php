48789066fd7a6c4473f6cbf364e2ed39e15d5e9f
misu571 <jubir.hosn@gmail.com>
2026-02-14 18:43:03 +0600

    added payment type resource

---
+++ app/Filament/Resources/PaymentTypes/Pages/ManagePaymentTypes.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\PaymentTypes\Pages;
+
+use App\Filament\Resources\PaymentTypes\PaymentTypeResource;
+use Filament\Actions\CreateAction;
+use Filament\Resources\Pages\ManageRecords;
+use Filament\Support\Enums\Width;
+
+class ManagePaymentTypes extends ManageRecords
+{
+    protected static string $resource = PaymentTypeResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make()
+                ->label('Add record')
+                ->modalHeading('Create record')
+                ->modalWidth(Width::Medium),
+        ];
+    }
+}
